4ab3f3131441b6c5c599b0df3cd2a810840b64c0
AkramDadKhan1 <akram.khan@fyntune.com>
2023-10-01 15:34:18 +0530

    Routing Changes

---
+++ myapp/app/login/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Login = () => {
+    return (
+        <div>
+           <Link href="/">Go to Home page</Link>
+        </div>
+    );
+};
+
+export default Login;
--- myapp/app/page.js
+++ myapp/app/page.js
@@ -1,8 +1,22 @@
-import styles from './page.module.css'
+"use client"
+import Link from "next/link";
+import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
 export default function Home() {
+  const router = useRouter();
   return (
-    <main className={styles.main}>
-<h1>Hello</h1>
+    <main >
+      <h1>Hello</h1>
+      <Link href="/login">Go to Login page</Link>
+      <br/>
+      <br/>
+      <Link href="about">Go to About page</Link>
+      <br/>
+      <br/>
+      <button onClick={()=>router.push("/login")}>Go to Login Page</button>
+      <br/>
+      <br/>
+      <button onClick={()=>router.push("/about")}>Go to About Page</button>
     </main>
-  )
+  );
 }
